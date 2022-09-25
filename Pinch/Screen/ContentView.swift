c751762fcf7d9f2caeb3b6df503f825205221856
Baris Ciftci <barishciftci@gmail.com>
2022-09-25 16:49:36 +0200

    Pages are added.

---
+++ Pinch/Screen/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Baris Ciftci on 25/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    // MARK: PROPERTIES
+    
+    // MARK: FUNCTION
+    
+    // MARK: CONTENT
+    
+    var body: some View {
+        VStack {
+            
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+// MARK: PREVIEW
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 14")
+            .preferredColorScheme(.dark)
+    }
+}
