3496ea69bccc6e29ed9825ec9f437042530702c0
Baris Ciftci <barishciftci@gmail.com>
2022-09-25 17:38:25 +0200

    Double Tap Gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     
     // MARK: PROPERTIES
+    @State private var isAnimating :  Bool = false
+    @State private var imageScale :  CGFloat = 1
     
     // MARK: FUNCTION
     
@@ -18,7 +20,40 @@ struct ContentView: View {
     var body: some View {
         VStack {
             
-            Text("Hello, world!")
+            NavigationView{
+                ZStack {
+                    // MARK: PAGE IMAGE
+                    Image("magazine-front-cover")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(20)
+                        .padding()
+                        .shadow(color: .black.opacity(0.2), radius: 24, x: 2, y: 2)
+                        .opacity(isAnimating ? 1: 0)
+                        .scaleEffect(imageScale)
+                    //MARK: - 1. TAP GESTURE
+                        .onTapGesture(count: 2, perform: {
+                            if imageScale == 1 {
+                                withAnimation(.spring()) {
+                                    imageScale = 5
+                                }
+                            } else {
+                                withAnimation(.spring()) {
+                                    imageScale = 1
+                                }
+                            }
+                        })
+                }//: ZSTACK
+                .navigationTitle("Pinch & Zoom")
+                .navigationBarTitleDisplayMode(.inline)
+                .onAppear(perform: {
+                    withAnimation(.linear(duration: 0)) {
+                        isAnimating = true
+                    }
+                })
+            }//: NAVIGATION
+            .navigationViewStyle(.stack)
+            
         }
         .padding()
     }
